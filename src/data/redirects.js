ceb9b14615daa6675f3dc5e1ceb415ccfd0b570a
Vijay Reshamwala <rvijay@alita.local>
2023-09-06 16:45:59 +0530

    prepare data

---
+++ src/data/redirects.js
@@ -0,0 +1,33 @@
+export const redirects =[
+    {
+        redirId:'1',
+        redirName:'RedirHub Name line',
+        redirSource:'console.dnspod.cn/dns/djiankong.com/record',
+        redirDestination:'https://www.redirhub.com',
+        redirLogo:'redirhub',
+        redirTags:['Personal','New'],
+        redirClicks:4,
+        redirDate:'16-08-2023'
+    },
+    {
+        redirId:'2',
+        redirName:'Facebook redirect',
+        redirSource:'console.dnspod.cn/dns/djiankong.com/record',
+        redirDestination:'https://www.redirhub.com',
+        redirLogo:'facebook',
+        redirTags:[],
+        redirClicks:2,
+        redirDate:'05-09-2023'
+    },
+    {
+        redirId:'3',
+        redirName:'Loom redirect',
+        redirSource:'console.dnspod.cn/dns/djiankong.com/record',
+        redirDestination:'https://www.redirhub.com',
+        redirLogo:'globe',
+        redirTags:['Personal'],
+        redirClicks:1,
+        redirDate:'10-08-2023'
+    },
+    
+]
